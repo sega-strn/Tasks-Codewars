64ba0966d3fbecf562abb30747d66e772962c8c9
Sergey <segadev85@gmail.com>
2024-06-23 01:07:01 +0300

    "first commit"

---
+++ 8 kyu/sumOfPositive.js
@@ -0,0 +1,26 @@
+/*
+-- Sum of Positive --
+
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+*/
+
+
+function positiveSum(arr) {
+    // let sum = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] > 0) {
+    //         sum += arr[i]
+    //     }
+    // }
+    // return sum
+
+    // вариант 2 (reduce)
+    let res = arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+    return res
+}
+
+console.log(positiveSum(([1, 2, 3, 4, 5]))) // 15
+console.log(positiveSum(([1, 2, 3, -4, 5]))) // 11
+console.log(positiveSum(([1, -2, 3, -4, 5]))) // 9
+console.log(positiveSum(([1, 10, 3, -4, 5]))) // 9
